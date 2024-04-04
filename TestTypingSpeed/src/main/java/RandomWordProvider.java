78546bcdbcbbd8fe1af00270e5b96c725859c71e
Olliewakeford <Oliverwakeford2001@gmail.com>
2024-04-04 17:24:18 +0200

    Logic and text to generate a list of 200 random words

--- TestTypingSpeed/src/main/java/RandomWordProvider.java
+++ TestTypingSpeed/src/main/java/RandomWordProvider.java
@@ -1,7 +1,8 @@
 package main.java;
 
-import java.util.List;
-import java.util.Arrays;
+import java.util.*;
+import java.nio.file.*;
+import java.io.IOException;
 
 // Implementation of WordProvider to provide random words for user to type
 // Random words do not form sentences which make sense
@@ -9,10 +10,14 @@ import java.util.Arrays;
 public class RandomWordProvider implements WordProvider {
     @Override
     public List<String> getWords() {
-        // Implement logic to get random words
-        //This can be from a file or online database
+        List<String> words = new ArrayList<>();
+        try {
+            words = Files.readAllLines(Paths.get(System.getProperty("user.dir") + "/resources/RandomWords.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        // For now, returning a list of random words
-        return Arrays.asList("word1", "word2", "word3");
+        Collections.shuffle(words);
+        return words.subList(0, Math.min(words.size(), 200));
     }
 }
