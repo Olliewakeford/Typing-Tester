dfdb49751605945e2a453f2d8bc89b6a5b42235c
Olliewakeford <Oliverwakeford2001@gmail.com>
2024-04-04 14:59:51 +0200

    Basic structure of main method

--- TestTypingSpeed/src/main/java/Main.java
+++ TestTypingSpeed/src/main/java/Main.java
@@ -1,6 +1,38 @@
+package main.java;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Select an option for the typing test:");
+        System.out.println("1. Random words");
+        System.out.println("2. Difficult words");
+        System.out.println("3. Normal sentences");
+
+        Scanner scanner = new Scanner(System.in);
+        int option = scanner.nextInt();
+
+        //different kind of words/sentences for user to type
+        WordProvider wordProvider;
+        switch (option) {
+            case 1:
+                wordProvider = new RandomWordProvider();
+                break;
+            case 2:
+                wordProvider = new DifficultWordProvider();
+                break;
+            case 3:
+                wordProvider = new NormalSentenceProvider();
+                break;
+            default:
+                System.out.println("Invalid option. Exiting...");
+                return;
+        }
+
+        TypingTest typingTest = new TypingTest(wordProvider);
+        Result result = typingTest.startTest();
+
+        System.out.println("Your typing speed: " + result.getSpeed());
+        System.out.println("Your typing accuracy: " + result.getAccuracy());
     }
 }
