8f784c3e3ba60a4daf3add37ee7ede024127ac9b
Olliewakeford <Oliverwakeford2001@gmail.com>
2024-04-10 12:41:48 +0200

    Add more detail to the Result

--- TestTypingSpeed/src/main/java/CommandLineTypingTest/Result.java
+++ TestTypingSpeed/src/main/java/CommandLineTypingTest/Result.java
@@ -3,10 +3,14 @@ package CommandLineTypingTest;
 public class Result {
     private final int speed;
     private final int accuracy;
+    private final int totalWords;
+    private final int correctWords;
 
-    public Result(int speed, int accuracy) {
+    public Result(int speed, int accuracy, int totalWords, int correctWords) {
         this.speed = speed;
         this.accuracy = accuracy;
+        this.totalWords = totalWords;
+        this.correctWords = correctWords;
     }
 
     public int getSpeed() {
@@ -15,5 +19,13 @@ public class Result {
 
     public int getAccuracy() {
         return accuracy;
+    }
+
+    public int getTotalWords() {
+        return totalWords;
+    }
+
+    public int getCorrectWords() {
+        return correctWords;
     }
 }
