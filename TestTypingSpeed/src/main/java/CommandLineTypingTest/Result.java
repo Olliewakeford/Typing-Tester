5ad8274ec804ee489d45f91b955652d1e5e2b1f4
Olliewakeford <Oliverwakeford2001@gmail.com>
2024-04-11 13:42:16 +0200

    Add toString method to result

--- TestTypingSpeed/src/main/java/CommandLineTypingTest/Result.java
+++ TestTypingSpeed/src/main/java/CommandLineTypingTest/Result.java
@@ -1,7 +1,7 @@
 package CommandLineTypingTest;
 
 
-public record Result(long testTime, int totalWords, int correctWords) {
+public record Result(long testTime, int correctWords, int totalWords) {
 
     // Method to calculate the speed of the user as words per minute
     public int calculateSpeed() {
@@ -14,6 +14,18 @@ public record Result(long testTime, int totalWords, int correctWords) {
         return (int) ((double) correctWords / totalWords * 100);
     }
 
+    // Method to display the result of the test
+    @Override
+    public String toString() {
+        double testTimeInMinutes = testTime / 60000.0;
+        int speed = calculateSpeed();
+        int accuracy = calculateAccuracy();
 
-
+        return "Test Duration: " + testTimeInMinutes + " minutes\n" +
+                "Total Words: " + totalWords + "\n" +
+                "Correct Words: " + correctWords + "\n" +
+                "Speed: " + speed + " words per minute\n" +
+                "Accuracy: " + accuracy + "%\n";
+    }
 }
+
